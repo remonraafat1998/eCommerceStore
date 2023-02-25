3fffceb0aaad8cfb74202043e121731829aaa702
remonraafat1998 <remonraafat1998@gmail.com>
2023-02-25 14:02:56 +0200

    update

--- src/app/modules/shared/components/start-rating/start-rating.component.ts
+++ src/app/modules/shared/components/start-rating/start-rating.component.ts
@@ -1,15 +1,27 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-start-rating',
   templateUrl: './start-rating.component.html',
   styleUrls: ['./start-rating.component.scss']
 })
-export class StartRatingComponent  implements OnInit{
+export class StartRatingComponent implements OnInit {
 
- ngOnInit(): void {
-
- }
-
+  ngOnInit(): void {
+    this.chooseStars();
+  }
+  chooseStars() {
+    let stars = document.querySelectorAll('.rating-btn')
+    stars.forEach((star, index) => {
+      star.addEventListener('click', () => {
+        stars.forEach(ele => {
+          ele.classList.remove('orange', 'checked')
+          for (let i = 0; i <= index; i++) {
+            stars[i].classList.add('orange', 'checked')
+          }
+        })
+      })
+    })
+  }
 
 }
